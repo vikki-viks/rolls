3530bfa250c85d266db2608ffb9fcb34a65be45b
vikki-viks <vicka.kudryavtseva@yandex.ru>
2021-06-23 21:02:10 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { CartImage } from './components/CartImage';
+import { Logo } from './components/Logo';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: auto;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+`;
+
+const TitleWrapper = styled.div`
+  margin-left: 16px;
+`;
+
+const CartButton = styled.button`
+  display: block;
+  border-radius: 17px;
+  background-color: #ff694a;
+  padding: 7px 17px;
+  padding-left: 40px;
+  outline: 0;
+  border: 0;
+  position: relative;
+  text-align: left;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <div>
+          <LogoWrapper>
+            <Logo></Logo>
+
+            <TitleWrapper>
+              <div>REACT ROLLS</div>
+              <div>самые вкусные роллы во вселенной</div>
+            </TitleWrapper>
+          </LogoWrapper>
+        </div>
+
+        <CartButton>
+          <CartImage></CartImage> 1347
+        </CartButton>
+      </Header>
     </div>
   );
 }
