89986243f97e808d24261f064eb14cff11ed7707
vikki-viks <vicka.kudryavtseva@yandex.ru>
2022-07-10 19:03:01 +0300

    create request

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,2 +1,11 @@
 const Router = require("express");
 const router = new Router();
+const rollsRouter = require("./rollsRouter");
+const typeRouter = require("./typeRouter");
+const userRouter = require("./userRouter");
+
+router.use("/user", userRouter);
+router.use("/type", typeRouter);
+router.use("/rolls", rollsRouter);
+
+module.exports = router;
