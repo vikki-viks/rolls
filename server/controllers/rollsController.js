77014682c3a4517fd62376163e436aa930a951c6
vikki-viks <vicka.kudryavtseva@yandex.ru>
2022-07-13 12:03:51 +0300

    create getAll and info

--- server/controllers/rollsController.js
+++ server/controllers/rollsController.js
@@ -1,17 +1,27 @@
 const uuid = require("uuid");
 const path = require("path");
-const { Rolls } = require("../models/models");
+const { Rolls, RollsInfo } = require("../models/models");
 const ApiError = require("../error/ApiError");
 class RollsController {
   async create(req, res, next) {
     try {
-      const { name, typeId } = req.body;
+      let { name, typeId, info } = req.body;
       console.log("here");
       const { img } = req.files;
       let fileName = uuid.v4() + ".jpg";
       img.mv(path.resolve(__dirname, "..", "static", fileName));
-
       const rolls = await Rolls.create({ name, typeId, img: fileName });
+
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((i) => {
+          RollsInfo.create({
+            title: i.title,
+            description: i.description,
+            rollsId: rolls.id,
+          });
+        });
+      }
       return res.json(rolls);
     } catch (e) {
       console.log(e);
@@ -19,7 +29,16 @@ class RollsController {
     }
   }
   async getAll(req, res) {
-    const rolls = await Rolls.findAll();
+    let { typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let rolls;
+    if (!typeId) {
+      rolls = await Rolls.findAndCountAll({ limit, offset });
+    } else {
+      rolls = await Rolls.findAndCountAll({ where: { typeId }, limit, offset });
+    }
     return res.json(rolls);
   }
   async getOne(req, res) {}
