28179562694b33dfb74eb41a17c902e934a59677
vikki-viks <vicka.kudryavtseva@yandex.ru>
2022-07-14 17:18:12 +0300

    create auth and check middleware

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const typeController = require("../controllers/typeController");
+const checkMiddleware = require("../middleware/CheckRoleMiddleware");
 
-router.post("/", typeController.create);
+router.post("/", checkMiddleware("ADMIN"), typeController.create);
 router.get("/", typeController.getAll);
 
 module.exports = router;
